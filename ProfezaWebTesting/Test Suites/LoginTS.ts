585ca4165d9bcb20513194c638e1f5c4e4df3957
Unknown <anjali.mca17.du@gmail.com>
2018-07-01 23:54:15 +0530

    Login Test Suite.

---
+++ ProfezaWebTesting/Test Suites/LoginTS.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-07T14:49:55</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>120</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>872515a4-636c-465f-8a54-b8fb9b9320f0</testSuiteGuid>
+   <testCaseLink>
+      <guid>05e4d188-4a07-4e68-8a64-2980db0692f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/T1_nodetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>940da87c-7838-4249-8552-548e677208c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/T2_invalidemail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c860ecc8-514b-4372-a3cb-f6e4c7b9ff56</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/T3_unregisteredemail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2888c78b-309d-4e07-b88b-412ace1d3f0e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/T4_registeredemail_nopassword</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2e9faef-89a4-45f3-8b87-6fa43566dbf9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/T5_registeredemail_shortpassword</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aee94484-e2bd-486f-9ca1-0a5f468dcb03</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/T6_registeredemail_wrongpassword</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8fe11f32-a5fa-441f-a755-13e4e21a6e33</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/T7_correctdetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
