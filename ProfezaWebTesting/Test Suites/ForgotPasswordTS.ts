9bb608adec266c81a5821c4ea8e3ea5d57e4d7e5
Unknown <anjali.mca17.du@gmail.com>
2018-07-04 23:58:30 +0530

    Test Suite Forgot Password Module.

---
+++ ProfezaWebTesting/Test Suites/ForgotPasswordTS.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ForgotPasswordTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-07T17:28:30</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>120</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>bbdad7be-a886-483d-bc35-ff326974a517</testSuiteGuid>
+   <testCaseLink>
+      <guid>9e0e9ab0-f8a2-4080-b2eb-0a5f65a23810</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/T1_correctemail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8118634a-25be-4561-acc6-271f1872c17a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/T2_unregisteredmail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9c4d7a9d-f65a-452a-ad7e-a97c4d22e9e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/T3_registeredmail_invalidcode</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2d9f3ff-0b3d-4dd9-a4c0-aa834b724aac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/T4_registeredmail_validcode</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
