a20d8decbf0880d81e10a2bb0b4ef5ccb4a67da1
Unknown <anjali.mca17.du@gmail.com>
2018-07-06 00:05:02 +0530

    My Profile Test Suite.

---
+++ ProfezaWebTesting/Test Suites/MyProfileTS.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MyProfileTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-18T11:58:02</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>180</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>45d97333-d5b0-450c-8ec7-a0b5272ce567</testSuiteGuid>
+   <testCaseLink>
+      <guid>f02ec523-211d-4962-93ed-5b82c0b15a56</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MyProfile/T1_UpdatingDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
