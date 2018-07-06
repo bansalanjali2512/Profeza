720f9b0aa80ee393d0c3595d12583177952b1ec5
Unknown <anjali.mca17.du@gmail.com>
2018-07-07 00:00:19 +0530

    SignUp module test suite.

---
+++ ProfezaWebTesting/Test Suites/SignUpTS.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SignUpTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-18T11:59:00</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>214d5709-7b2d-4937-8231-efc31e2ff90c</testSuiteGuid>
+   <testCaseLink>
+      <guid>e156050b-802b-4f75-84cd-21b8e87c723c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/T1_NoDetailsEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c9a6fab8-28c8-46b6-b712-a5abf160706c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/T2_NoEmailEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8de76fd-68fa-4eaf-a66d-f06ae179eb70</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/T3_NoNameEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>780f93fa-8936-43da-9ec2-b11f81367e3a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/T4_NoPasswordEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa6c2149-f4e9-420f-b5bf-d8b75be5ceff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/T5_ShortPassword</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8ea9902-6005-44c9-9c60-aad192d4c520</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/T6_RegisteredEmail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>22bb47ad-1f2a-4e29-b1c6-b026e59e4d28</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/T7_InvalidEmail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>541a2001-c63f-4170-adca-9d052e483f8f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/T8_ValidDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
