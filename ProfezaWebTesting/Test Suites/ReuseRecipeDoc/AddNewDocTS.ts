562c336bbbec8d7e23d12c1741caa843b024e90a
Unknown <anjali.mca17.du@gmail.com>
2018-07-08 23:55:35 +0530

    Add new doc test suite.

---
+++ ProfezaWebTesting/Test Suites/ReuseRecipeDoc/AddNewDocTS.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AddNewDocTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8e0627d3-5f2c-45dc-806a-e9e3c2747b7d</testSuiteGuid>
+   <testCaseLink>
+      <guid>caaefb30-214a-4f67-89c2-cc12c8d3fd29</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/AddNewDoc/T1_NoDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>124fef7d-e85e-4b9e-9be2-45464b492e3e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/AddNewDoc/T2_AllDetailsEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0b01c713-d8ce-4f57-8b94-fdeabb7dd1b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/AddNewDoc/T3_MIDNotEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e5330ab3-e3c6-4b37-9bea-701b6f055440</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/AddNewDoc/T4_TitleNotEntered</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
