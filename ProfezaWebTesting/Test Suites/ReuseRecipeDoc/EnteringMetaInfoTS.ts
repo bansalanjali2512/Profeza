d948fc6e18c132a6fd7840795a5aff1b93ee4aa1
Unknown <anjali.mca17.du@gmail.com>
2018-07-08 23:55:59 +0530

    Entering meta info test suite.

---
+++ ProfezaWebTesting/Test Suites/ReuseRecipeDoc/EnteringMetaInfoTS.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>EnteringMetaInfoTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>add47eab-c5de-4e38-ae33-d20ddcc3560d</testSuiteGuid>
+   <testCaseLink>
+      <guid>6e4f1cc4-833c-48e1-8d3b-5fb57daef1e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/EnteringMetaInfo/T1_NoDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa8fda07-9ae6-4292-8bae-e8e947700171</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/EnteringMetaInfo/T2_MIDEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f7e9ba2-70b2-4511-908a-7eb159baf83a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/EnteringMetaInfo/T3_MIDTitleEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e5dfce3-2778-4dd9-967f-4e2e90bb9768</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/EnteringMetaInfo/T4_MIDTitleExpNoEntered</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>695faf75-86a9-42b4-8d88-c6df8881f0b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReuseRecipeDoc/EnteringMetaInfo/T5_AllDetailsEntered</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
