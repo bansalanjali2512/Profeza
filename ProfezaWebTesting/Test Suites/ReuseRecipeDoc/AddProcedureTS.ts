93da8334e176356baaf256cf13a86cfda7499f01
Unknown <anjali.mca17.du@gmail.com>
2018-07-14 23:57:50 +0530

    Add procedure module tes suite.

---
+++ ProfezaWebTesting/Test Suites/ReuseRecipeDoc/AddProcedureTS.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AddProcedureTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4d85a27d-6560-4210-a30f-b1375499b4b7</testSuiteGuid>
+</TestSuiteEntity>
